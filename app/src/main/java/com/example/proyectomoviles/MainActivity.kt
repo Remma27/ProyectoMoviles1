b64e7813908b5eff4fece1cb8a1b8b86a87a1473
Remma27 <emmanuelrsolano27@gmail.com>
2023-11-15 10:53:14 -0600

    Login y Sign Up

--- app/src/main/java/com/example/proyectomoviles/MainActivity.kt
+++ app/src/main/java/com/example/proyectomoviles/MainActivity.kt
@@ -9,10 +9,23 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.proyectomoviles.databinding.ActivityMainBinding
 
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import com.example.proyectomoviles.ui.users.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
+
+const val valorIntentLogin = 1
+
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+
+    var auth = FirebaseAuth.getInstance()
+    var email: String? = null
+    var contra: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -31,5 +44,30 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // intenta obtener el token del usuario del local storage, sino llama a la ventana de registro
+        val prefe = getSharedPreferences("appData", Context.MODE_PRIVATE)
+        email = prefe.getString("email","")
+        contra = prefe.getString("contra","")
+
+        if(email.toString().trim { it <= ' ' }.length == 0){
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivityForResult(intent, valorIntentLogin)
+        }else {
+            val uid: String = auth.uid.toString()
+            if (uid == "null"){
+                auth.signInWithEmailAndPassword(email.toString(), contra.toString()).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Toast.makeText(this,"Autenticación correcta", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            obtenerDatos()
+        }
+
+    }
+
+    private fun obtenerDatos() {
+        Toast.makeText(this,"Esperando hacer algo importante", Toast.LENGTH_LONG).show()
     }
 }
